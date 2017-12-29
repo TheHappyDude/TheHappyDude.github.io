43de261ba0e346aaae710071928d8a3d9136642d
kwang329 <kwang329@gatech.edu>
2017-12-28 20:36:51 -0500

    Carousel added

---
+++ res/scripts/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(".carousel").slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+    
+  });
+              
