c8ff3cc3112799f862241b0204cfcb395b1a64c3
kwang329 <kwang329@gatech.edu>
2017-12-30 23:23:19 -0500

    More fixes to cards, minor CSS adjustments, footer work

--- res/scripts/scripts.js
+++ res/scripts/scripts.js
@@ -3,10 +3,30 @@ $(document).ready(function () {
         dots: false,
         infinite: true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2500,
         speed: 1000,
-        slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+          ]
     });
 
 });
